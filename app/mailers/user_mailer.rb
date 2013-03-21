cfba4dd30e709e84a2ee555a105326f0aa6a036e
Matt Goggin <goggin13@gmail.com>
2013-03-21 16:39:00 -0500

    add from

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,13 +3,17 @@ class UserMailer < ActionMailer::Base
   
   def welcome(user)
     @user = user
-    mail(to: user.email, subject: "Welcome to the Info2310 MicroPoster")
+    mail(to: user.email, 
+         subject: "Welcome to the Info2310 MicroPoster",
+         from: 'goggin13@gmail.com')
   end
   
   def mentioned(micro_post, mentioned_user)
     @mentioned_user = mentioned_user
     @micro_post = micro_post
-    mail(to: mentioned_user.email, subject: "You were mentioned")
+    mail(to: mentioned_user.email, 
+         subject: "You were mentioned",
+         from: 'goggin13@gmail.com')
   end
 end
 
